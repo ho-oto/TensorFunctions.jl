70dcbc0b1177e42388061d33b2c4dc3314b5a26c
ho-oto <nakanodesu@gmail.com>
2019-06-04 20:55:18 +0900

    remove tensormap

--- src/TensorFunctions.jl
+++ src/TensorFunctions.jl
@@ -4,13 +4,21 @@ using TensorOperations
 using LinearMaps
 using TensorCast
 
-export @tensorfunc, @tensormap
+export @tensorfunc
 
-macro tensorfunc(ex::Expr)
-    ex
+'''
+f(A,B) = @tensorfunc[(:a,:b),:d] (:a=1,:b=3,:c=2) (A[:a,(:b,:c)] :b=2) * B[:c,:d]
+<=>
+function f(A,B)
+    @cast Aprime[a,b,c] := A[a,(b,c)] b:2
+    @cast Bprime[c,d] := B[c,d]
+    @tensoropt (a=>chi,b=>chi^2,c=>chi) tmp[a,b,d] := Aprime[a,b,c] * Bprime[c,d]
+    @cast res[(a,b),d] = tmp[a,b,d]
 end
 
-macro tensormap(ex::Expr)
+
+'''
+macro tensorfunc(ex::Expr)
     ex
 end
 

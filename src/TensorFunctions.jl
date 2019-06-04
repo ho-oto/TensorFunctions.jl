0be8b73e4cf3b0d8d24a9709ddfb3c4df11350d9
ho-oto <nakanodesu@gmail.com>
2019-06-04 20:10:32 +0900

    rough cast

--- src/TensorFunctions.jl
+++ src/TensorFunctions.jl
@@ -1,5 +1,17 @@
 module TensorFunctions
 
-greet() = print("Hello World!")
+using TensorOperations
+using LinearMaps
+using TensorCast
+
+export @tensorfunc, @tensormap
+
+macro tensorfunc(ex::Expr)
+    ex
+end
+
+macro tensormap(ex::Expr)
+    ex
+end
 
 end # module

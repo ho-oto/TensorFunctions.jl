f94a2c7c236b78dc01c974952ad463f67eec07cb
ho-oto <nakanodesu@gmail.com>
2019-06-05 15:31:41 +0900

    add remove_quote

--- src/TensorFunctions.jl
+++ src/TensorFunctions.jl
@@ -3,6 +3,35 @@ module TensorFunctions
 using TensorOperations
 using TensorCast
 
+function remove_quote(ex::Expr)
+    if ex.head == :ref
+        exout = Expr(:ref,ex.args[1])
+        for elem in ex.args[2:end]
+            if typeof(elem) == QuoteNode
+                push!(exout.args,elem|>eval)
+            elseif typeof(elem) == Expr && elem.head == :tuple
+                push!(exout.args,remove_quote(elem))
+            else
+                error("ex.args should be Symbol or tuple of Symbols")
+            end
+        end
+    elseif ex.head == :vect || ex.head == :tuple
+        exout = Expr(ex.head)
+        for elem in ex.args
+            if typeof(elem) == QuoteNode
+                push!(exout.args,elem|>eval)
+            elseif typeof(elem) == Expr && elem.head == :tuple
+                push!(exout.args,remove_quote(elem))
+            else
+                error("ex.args should be Symbol or tuple of Symbols")
+            end
+        end
+    else
+        error("ex.head should be [:ref,:vect,:tuple]")
+    end
+    exout
+end
+
 function quotenode_to_symbol(ex::Expr)
     if !(ex.head == :ref); error("ex should be :(Foo[:a,(:b,:c)])"); end
     exout = Expr(:ref,ex.args[1])
@@ -111,6 +140,35 @@ function _tensorfunc(ind::Expr,ex::Expr,ncon::Expr)
     end
     newsym = gensym()
     push!(outex.args,TensorOperations.tensorify(Expr(:(:=),Expr(:ref,newsym,Symbol(":")),exx)))
+    ressym = gensym()
+    push!(outex.args,TensorCast._macro(Expr(:(:=),quotenode_to_symbol_(ind,ressym),remove_bracket_(ind,newsym))))
+    outex
+end
+
+function _tensorfunc(ind::Expr,ex::Expr)
+    if !(ind.head == :vect); error("expected to be :vect"); end
+    if !(ex.head == :call); error("expected to be :call"); end
+    if !(ex.args[1] == :*); error("only * is supported"); end
+
+    outex = Expr(:block)
+    exx = copy(ex)
+
+    for i in 2:length(ex.args)
+        if ex.args[i].head == :ref
+            exx.args[i] = quotenode_to_symbol(ex.args[i])
+        elseif ex.args[i].head == :tuple
+            newsym = gensym()
+            rhs = quotenode_to_symbol(ex.args[i].args[1])
+            lhs = remove_bracket(rhs,newsym)
+            tmpargs = [set_size(j) for j in ex.args[i].args[2:end]]
+            push!(outex.args,TensorCast._macro(Expr(:(:=),lhs,rhs),tmpargs...))
+            exx.args[i] = lhs
+        else
+            error("???")
+        end
+    end
+    newsym = gensym()
+    push!(outex.args,TensorOperations.tensorify(Expr(:(:=),remove_bracket_(ind,newsym),exx)))
     ressym = gensym()
     push!(outex.args,TensorCast._macro(Expr(:(:=),quotenode_to_symbol_(ind,ressym),remove_bracket_(ind,newsym))))
     outex

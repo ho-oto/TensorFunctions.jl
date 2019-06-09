80c02a4ee0c147ea24a8775ffacb69bfca2b4b68
ho-oto <nakanodesu@gmail.com>
2019-06-10 02:42:23 +0900

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,19 @@ using TensorFunctions
 using Test
 
 @testset "TensorFunctions.jl" begin
-    @test true
-    @test_nowarn println("test")
+    @test TensorFunctions.isinpairedindex(:( :a ),:lhs) == true
+    @test TensorFunctions.isinpairedindex(:( :b|1 ),:lhs) == false
+    @test TensorFunctions.isinpairedindex(:( :b|size(foo,1) ),:lhs) == false
+    @test TensorFunctions.isinpairedindex(:( :a ),:rhs) == true
+    @test TensorFunctions.isinpairedindex(:( :b|1 ),:rhs) == true
+    @test TensorFunctions.isinpairedindex(:( :b|size(foo,1) ),:rhs) == true
+
+    @test TensorFunctions.ispairedindex(:( :a ),:lhs) == false
+    @test TensorFunctions.ispairedindex(:( (:a,:b) ),:lhs) == true
+    @test TensorFunctions.ispairedindex(:( (:a,:b|1) ),:lhs) == false
+    @test TensorFunctions.ispairedindex(:( (:a|foo(bar),:b) ),:lhs) == false
+    @test TensorFunctions.ispairedindex(:( :a ),:rhs) == false
+    @test TensorFunctions.ispairedindex(:( (:a,:b) ),:rhs) == true
+    @test TensorFunctions.ispairedindex(:( (:a,:b|1) ),:rhs) == true
+    @test TensorFunctions.ispairedindex(:( (:a|foo(bar),:b) ),:rhs) == true
 end

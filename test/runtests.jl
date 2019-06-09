caafc2ed45e0eff6ca61fa339deb42b561a1444c
ho-oto <nakanodesu@gmail.com>
2019-06-10 03:17:53 +0900

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,10 @@ using TensorFunctions
 using Test
 
 @testset "TensorFunctions.jl" begin
+    @test TensorFunctions.issymbol(:( :b )) == true
+    @test TensorFunctions.issymbol(:( b )) == false
+    @test TensorFunctions.tosymbol(:( :b )) == :b
+
     @test TensorFunctions.isinpairedindex(:( :a ),:lhs) == true
     @test TensorFunctions.isinpairedindex(:( :b|1 ),:lhs) == false
     @test TensorFunctions.isinpairedindex(:( :b|size(foo,1) ),:lhs) == false
@@ -17,4 +21,9 @@ using Test
     @test TensorFunctions.ispairedindex(:( (:a,:b) ),:rhs) == true
     @test TensorFunctions.ispairedindex(:( (:a,:b|1) ),:rhs) == true
     @test TensorFunctions.ispairedindex(:( (:a|foo(bar),:b) ),:rhs) == true
+
+    @test TensorFunctions.isindexproduct(:( :a*5 )) == true
+    @test TensorFunctions.isindexproduct(:( :a*hoge() )) == false
+    @test TensorFunctions.isindexproduct(:( :a )) == false
+    @test TensorFunctions.isindexproduct(:( :a*5*5 )) == false
 end

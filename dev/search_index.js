f926e541ee6228933ed5fce6b6e620c99e5d1173
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-03 10:25:07 +0000

    build based on aa7c912

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TensorFunctions.jl-1","page":"Home","title":"TensorFunctions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [TensorFunctions]","category":"page"},{"location":"#TensorFunctions.@tensorfunc-Tuple{Expr,Expr}","page":"Home","title":"TensorFunctions.@tensorfunc","text":"@tensorfunc(optex, block)\n@tensorfunc(block)\n\nTODO: write docs\n\n\n\n\n\n","category":"macro"}]
+}
